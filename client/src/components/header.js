c94f206addf9e101052cb9c09f80b4c6d55384f8
Muramori71 <dannyroh2500@gmail.com>
2023-09-08 11:47:57 -0700

    added header, footer, and navigation components and added Parents, Teachers, and Students.js into pages

---
+++ client/src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import Navigation from './navigation'
+
+export default function Header() {
+  return  (
+      <header className="header">
+          <h1>Woodward High School</h1>
+         {/* <Navigation page={page} setPage={setPage}/>  */}
+    </header>
+  )
+}
