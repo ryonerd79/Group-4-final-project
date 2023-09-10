96be7562bab1ef63eb4d96e9e6b15ed0bbba57fd
Luke <121516961+Afrozez@users.noreply.github.com>
2023-09-09 17:52:48 -0700

    trying to fix merge conflictions

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,9 @@
 import React from "react";
-// import Navigation from './navigation'
 
 export default function Header() {
-  return  (
-      <header className="header">
-          <h1>Woodward High School</h1>
-         {/* <Navigation page={page} setPage={setPage}/>  */}
+  return (
+    <header className="header">
+        <h1>Parent Teacher Communication</h1>
     </header>
-  )
+  );
 }
