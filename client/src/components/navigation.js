f74ada2c2a5b949dc25550f5f13cc42fc4d01ab0
Muramori71 <dannyroh2500@gmail.com>
2023-09-10 14:32:16 -0700

    updated Header and navigation js

--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-function Navigation({page, setPage}) {
+export default function Navigation({ currentPage, handlePageChange }) {
   return (
-    <nav>
-     <a href="#" onClick={() => setPage('')} className={page===''?'active':''}>About</a>
-     <a href="#" onClick={() => setPage('portfolio')} className={page==='students'?'active':''}>Students</a>
-     <a href="#" onClick={() => setPage('contact')} className={page==='parents'?'active':''}>Parents </a>
-     <a href="#" onClick={() => setPage('resume')} className={page==='teachers'?'active':''}>Teachers</a>
-    </nav>
-  )
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange("About")}
+          className={currentPage === "About" ? "active" : ""}
+          id="aboutId"
+        >
+          About me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "active" : ""}
+          id="contactId"
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#signup"
+          onClick={() => handlePageChange("Signup")}
+          className={currentPage === "Signup" ? "active" : ""}
+          id="signupId"
+        >
+          Signup
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#login"
+          onClick={() => handlePageChange("Login")}
+          className={currentPage === "Login" ? "active" : ""}
+          id="loginId"
+        >
+          Login
+        </a>
+      </li>
+    </ul>
+  );
 }
-
-export default Navigation
