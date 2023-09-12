aa237ea4d3214ee022dac428384530ef4973fc44
Muramori71 <dannyroh2500@gmail.com>
2023-09-12 11:35:17 -0700

    Updated Teachers.js in pages, added postSeeds.json to seeders folder, and added Post.js under models

---
+++ server/models/Post.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const PostSchema = new Schema({
+    postText: {
+      type: String,
+      required: 'You need to type a post!',
+      minlength: 1,
+      maxlength: 280,
+      trim: true,
+    },
+    postAuthor: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+      {
+        commentText: {
+          type: String,
+          required: true,
+          minlength: 1,
+          maxlength: 280,
+        },
+        commentAuthor: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (timestamp) => dateFormat(timestamp),
+        },
+      },
+    ],
+  });
+  
+  const Post = model('Post', PostSchema);
+  
+  module.exports = Post;
