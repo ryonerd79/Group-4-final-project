4e57e80ee393720b94286b8a2cbaab07e4a81d1e
Jake Norris <jacobnorris013@gmail.com>
2023-09-13 23:39:15 -0400

    fixed auth problems

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,6 +6,7 @@ const expiration = '2h';
 module.exports = {
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
+    token = token.replace('Bearer ', '')
 
     if (!token) {
       return req;
