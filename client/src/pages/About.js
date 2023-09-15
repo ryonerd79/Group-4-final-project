80f004cfaa03bd99bff38048e55e00bbd1982610
Muramori71 <dannyroh2500@gmail.com>
2023-09-14 20:13:11 -0700

    Updated summary in About.js and Added contact info in Contact.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,9 +2,14 @@ import React from "react";
 
 export default function About() {
     return (
+      <div>
+      <h1>About Us</h1>
       <p>
-        This app helps Teachers and Parents communicate about the academic progess and behavior pattern of the student they have relations with. 
+         Hi Parents and Teachers. We the teachtogether team have dedicated valuable time to create an app which will make communication between Teachers and Parents
+         convenient and pleasant. This app helps Teachers and Parents communicate about the academic progess and behavior pattern of the student they have relations with. 
+         This is also where announcements about school events can be posted. 
       </p>
+      </div>
     );
   }
   
