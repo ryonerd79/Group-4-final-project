bcda72176e085d701bf8808b6826bdc66d5b660b
Jake Norris <jacobnorris013@gmail.com>
2023-09-17 09:39:54 -0400

    added gradebook not done

--- client/src/pages/Gradebook.js
+++ client/src/pages/Gradebook.js
@@ -5,7 +5,7 @@ import { GET_GRADE_BOOK} from '../utils/queries'
 export default function About() {
     return (
       <p>
-        This app helps Teachers and Parents communicate about the academic progess and behavior pattern of the student they have relations with. 
+        Thids app helps Teachers and Parents communicate about the academic progess and behavior pattern of the student they have relations with. 
       </p>
     );
   }
