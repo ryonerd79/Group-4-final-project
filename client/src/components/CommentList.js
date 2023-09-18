04cd72ce09ed11dbb4ac71153c569b493187def1
dsoda86 <122141740+dsoda86@users.noreply.github.com>
2023-09-18 16:32:35 -0700

    deploy

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 const CommentList = ({ comments = [] }) => {
   if (!comments.length) {
-    return <h3>No Comments Yet. Get in the Conversation!</h3>;
+    return <h3 className='border bg-dark text-light text-center py-2'>No Comments Yet. Get in the Conversation!</h3>;
   }
 
   return (
     <>
       <h3
-        className="p-5 display-inline-block"
-        style={{ borderBottom: '1px dotted #1a1a1a' }}
+        className="p-3 display-inline-block bg-dark text-light border border-3 fs-2 d-flex justify-content-center"
       >
         Comments
       </h3>
@@ -17,14 +16,14 @@ const CommentList = ({ comments = [] }) => {
         {comments &&
           comments.map((comment) => (
             <div key={comment._id} className="col-12 mb-3 pb-3">
-              <div className="p-3 bg-dark text-light">
+              <div className="p-3 form-border bg-light-subtle">
                 <h5 className="card-header">
                   {comment.commentAuthor} commented{' '}
                   <span style={{ fontSize: '0.825rem' }}>
                     on {comment.createdAt}
                   </span>
                 </h5>
-                <p className="card-body">{comment.commentText}</p>
+                <p className="card-body mt-2 border-top border-dark pt-3">{comment.commentText}</p>
               </div>
             </div>
           ))}
